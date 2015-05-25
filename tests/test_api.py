df1cb80571c4ed135b0309ff9645586071baf90a
Michael Coelho <michael.tcoelho@gmail.com>
2015-05-25 15:36:54 -0300

    remove personal informations

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 
 class ApiTest(unittest.TestCase):
     def setUp(self):
-        self.api = PagarmeApi(endpoint=__endpoint__, api_key='ak_test_gyrZdScNg3V8u0YBA095uFpTMEUXct')
+        self.api = PagarmeApi(endpoint=__endpoint__, api_key='')
 
     def test_default_headers(self):
         self.assertIsInstance(self.api.default_headers, dict)
