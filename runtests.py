6741bddc0eea1818db0c376e1315cc321ab11db3
Michael Coelho <michael.tcoelho@gmail.com>
2015-05-28 18:15:06 -0300

    adding coveralls

--- runtests.py
+++ runtests.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 from nose.core import run
 
-if __name__ == "":
-    args = ['', ]
+if __name__ == "__main__":
+    args = ['', '--cover-package=pagarme', '--with-coverage']
     run(arg=args)
