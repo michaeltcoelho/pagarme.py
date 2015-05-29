ae741cf9e2faee0c56104d98a96ec7a2fcc94ff0
Michael Coelho <michael.tcoelho@gmail.com>
2015-05-29 15:56:03 -0300

    remove dateutil dependency

--- setup.py
+++ setup.py
@@ -1,17 +1,48 @@
-import setuptools
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from pagarme.config import __version__
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
-setuptools.setup(name='pagarme.py',
-                 version=__version__,
-                 description='Pagar.me Python API',
-                 long_description=open('README.md').read().strip(),
-                 author='Michael Coelho',
-                 author_email='michael.tcoelho@gmail.com',
-                 url='https://github.com/michaeltcoelho/pagarme.py',
-                 py_modules=['pagarme'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='pagarme python rest api',
-                 classifiers=['Packages'])
+readme = open('README.md').read().strip()
+requirements = ['requests', 'rsa']
+
+setup(
+    name='pagarme.py',
+    version=__version__,
+    description='Pagar.me Python API wrapper',
+    long_description=readme,
+    author='Michael Coelho',
+    author_email='michael.tcoelho@gmail.com',
+    url='https://github.com/michaeltcoelho/pagarme.py',
+    packages=[
+        'pagarme'
+    ],
+    package_dir={'pagarme': 'pagarme'},
+    include_package_data=True,
+    install_requires=requirements,
+    install_requires=requirements,
+    license='MIT License',
+    test_suite='runtests',
+    zip_safe=False,
+    keywords='pagarme, payment, gateway, api, wrapper',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
